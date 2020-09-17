3577853cb22806d02c31bd0a591be44a5ed7639a
Jun Kawashima <5635944+junkawa@users.noreply.github.com>
2020-09-17 14:29:39 +0900

    リファクタリング

--- content.js
+++ content.js
@@ -38,19 +38,6 @@ function _translateInnerHTMLRegexp(selector, map) {
   });
 }
 
-// data-tooltip 属性を日本語で置き換える
-function _translateDataTooltip(selector, map) {
-  const tooltips = document.querySelectorAll(selector);
-  // console.log(labels);
-  tooltips.forEach((tooltip) => {
-    const ret = map.find((m) => m['en'] ===
-    tooltip.getAttribute('data-tooltip')); // 先頭一致
-    if (ret) {
-      tooltip.setAttribute('data-tooltip', ret[defaultLanguage]);
-    }
-  });
-}
-
 // data-label 属性を日本語で置き換える
 function _translateDataLabel(selector, map) {
   const labels = document.querySelectorAll(selector);
@@ -66,195 +53,145 @@ function _translateDataLabel(selector, map) {
   });
 }
 
-function translateDynamicMainMenu() {
+function translateToolbar() {
   // メインメニューラベル要素の class
   // TODO 正規表現にして変更に耐性もたせるか? 速度低下する?
-  const selector = '.multilevel_dropdown--name--1abLT';
-  _translateInnerHTML(selector, mainMenuMap);
-}
+  _translateInnerHTML('.multilevel_dropdown--name--1abLT',
+      mainMenuMap);
 
-function translateDynamicTools() {
   // ツールラベル要素の class
-  const selector = '.action_option--text--3Rze3';
-  _translateInnerHTML(selector, toolsMap);
+  _translateInnerHTML('.action_option--text--3Rze3',
+      toolsMap);
 
-  // Done ボタン TODO map変数を独自に用意してもよいかも(mainMenuMapはおおきすぎるため)
-  const shareSelector = '.toolbar_view--textButton--eiCIw';
-  _translateInnerHTML(shareSelector, mainMenuMap);
+  // Done, Share ボタン TODO map変数を独自に用意してもよいかも(mainMenuMapはおおきすぎるため)
+  _translateInnerHTML('.toolbar_view--textButton--eiCIw',
+      mainMenuMap);
 }
 
 // 左(ページ)パネル
 // 右(プロパティ)パネル
-function translateDynamicPanel() {
+function translatePanel() {
   // パネルタイトル要素の class
-  {
-    const selector = '.raw_components--panelTitle--7MaOu';
-    _translateInnerHTML(selector, panelMap);
-    const selector2 = '.raw_components--panelTitle--7MaOu div';
-    _translateInnerHTML(selector2, panelMap);
-    const selector3 = '.draggable_list--panelTitleText--1q89R';
-    _translateInnerHTML(selector3, panelMap);
-    const selector4 = '.pages_panel--pagesHeaderText--1GE3u';
-    _translateInnerHTML(selector4, panelTabMap);
-    const selector5 = '.library_section_header--sectionHeader1_redesign--2tKT4';
-    _translateInnerHTML(selector5, panelTabMap);
-    const selector6 = '.component_tiles--sectionHeader--10N5o';
-    _translateInnerHTML(selector6, panelMap);
-    // パネル Layers, Assets, design, prototype, code
-    // Text Style編集パネルから戻ると Design, Prototype, Codeがオリジナル表示に戻るため
-    // ここで再度翻訳する
-    const selector7 = '.pages_panel--tab--3s1Y5';
-    _translateDataLabel(selector7, panelTabMap);
-  }
+  const panelTitleSelector =
+      '.raw_components--panelTitle--7MaOu,'+
+      '.raw_components--panelTitle--7MaOu div,'+
+      '.draggable_list--panelTitleText--1q89R,'+
+      '.component_tiles--sectionHeader--10N5o';
+  // label
+  const labelSelector =
+      '.raw_components--label--34YmO,'+
+      '.constraints_panel--checkboxLabel--3vvz3,'+
+      '.instance_panel__OLD--link--3yIYF,'+
+      '.basic_form--btn--3A-Ju,'+
+      '.raw_components--labelInactive--2GwUQ,'+
+      '.basic_form--label--hdhP9,'+
+      'a.style_preview_panel--addDescriptionLink--26D0-';
+  // 右クリック
+  const rightClickSelector = '.library_item_tile--contextMenu--N4uYI a';
+  _translateInnerHTML(
+      panelTitleSelector+','+labelSelector+','+rightClickSelector,
+      panelMap);
+
+  // パネルタイトル要素の class
+  _translateInnerHTML(
+      '.pages_panel--pagesHeaderText--1GE3u,'+
+        '.library_section_header--sectionHeader1_redesign--2tKT4',
+      panelTabMap);
+
+  // パネル Layers, Assets, design, prototype, code
+  // Text Style編集パネルから戻ると Design, Prototype, Codeがオリジナル表示に戻るため
+  // ここで再度翻訳する
+  _translateDataLabel(
+      '.pages_panel--tab--3s1Y5',
+      panelTabMap);
 
   // セレクトボックス
-  {
-    // 選択済み
-    const selector1 = '.select--inputText--6fenW';
-    _translateInnerHTML(selector1, panelSelectMap);
-    const selector2 = '.paint_panels--typeContainer--KyReT';
-    _translateInnerHTML(selector2, panelSelectMap);
-
-    // 選択メニュー
-    const selector3 = '.select--optionText--2C5M1';
-    _translateInnerHTML(selector3, panelSelectMap);
-    const selector4 = '.multilevel_dropdown--name--1abLT';
-    _translateInnerHTML(selector4, panelSelectMap);
-    const selector5 = '.prototype_device_panel--deviceOption--1zMFr span';
-    _translateInnerHTML(selector5, panelSelectMap);
-  }
-
-  // label
-  {
-    const selector1 = '.raw_components--label--34YmO';
-    _translateInnerHTML(selector1, panelMap);
-    const selector2 = '.constraints_panel--checkboxLabel--3vvz3';
-    _translateInnerHTML(selector2, panelMap);
-    const selector3 = '.instance_panel__OLD--link--3yIYF';
-    _translateInnerHTML(selector3, panelMap);
-    const selector4 = '.basic_form--btn--3A-Ju';
-    _translateInnerHTML(selector4, panelMap);
-    const selector5 = '.raw_components--labelInactive--2GwUQ';
-    _translateInnerHTML(selector5, panelMap);
-    const selector6 = '.basic_form--label--hdhP9';
-    _translateInnerHTML(selector6, panelMap);
-    const selector7 = 'a.style_preview_panel--addDescriptionLink--26D0-';
-    _translateInnerHTML(selector7, panelMap);
-  }
-
-  // 右クリック
-  {
-    const selector1 = '.library_item_tile--contextMenu--N4uYI a';
-    _translateInnerHTML(selector1, panelMap);
-  }
+  // 選択済み
+  const selectedSelector =
+  '.select--inputText--6fenW,'+
+  '.paint_panels--typeContainer--KyReT';
+  // 選択メニュー
+  const selectMenuSelector =
+  '.select--optionText--2C5M1,'+
+  '.multilevel_dropdown--name--1abLT,'+
+  '.prototype_device_panel--deviceOption--1zMFr span';
+  _translateInnerHTML(
+      selectedSelector+','+selectMenuSelector,
+      panelSelectMap);
 
   // Interaction
-  {
-    const selector1 = '.prototype_interaction_list--interactionName--3052S';
-    _translateInnerHTML(selector1, interactionListMap);
-    // フレーム名が翻訳されるため、削除
-    // const selector2 = '.prototype_interaction_list--targetText--M-vAz';
-    // _translateInnerHTML(selector2, interactionListMap);
-  }
+  _translateInnerHTML(
+      '.prototype_interaction_list--interactionName--3052S',
+      interactionListMap);
+  // フレーム名が翻訳されるため、削除
+  // _translateInnerHTML('.prototype_interaction_list--targetText--M-vAz',
+  //    interactionListMap);
 }
 
 // モーダルウィンドウ
-function translateDynamicModal() {
+function translateModal() {
   // 画像
-  {
-    const selector1 = '.image_settings_modal--colorAdjustLabel--2F676';
-    _translateInnerHTML(selector1, modalMap);
-    // 1行表示にするため
-    const labels = document.querySelectorAll(selector1);
-    labels.forEach((label) => {
-      label.style['grid-column-end'] = '12';
-    });
-  }
+  const imageSelector = '.image_settings_modal--colorAdjustLabel--2F676';
   // テキスト
-  {
-    const selector1 = '.header_modal--headerModalTitle--8hnpX';
-    _translateInnerHTML(selector1, modalMap);
-    const selector2 = '.type_settings--previewPlaceholder--mjwRc';
-    _translateInnerHTML(selector2, modalMap);
-    const selector3 = '.raw_components--label--34YmO';
-    _translateInnerHTML(selector3, modalMap);
-    const selector4 = '.raw_components--panelTitle--7MaOu';
-    _translateInnerHTML(selector4, modalMap);
-  }
+  const textSelector =
+        '.header_modal--headerModalTitle--8hnpX,'+
+        '.type_settings--previewPlaceholder--mjwRc,'+
+        '.raw_components--label--34YmO,'+
+        '.raw_components--panelTitle--7MaOu';
+  _translateInnerHTML(
+      imageSelector+','+textSelector,
+      modalMap);
+
+  // 1行表示にするため
+  const labels = document.querySelectorAll(imageSelector);
+  labels.forEach((label) => {
+    label.style['grid-column-end'] = '12';
+  });
 }
 
-function translateDynamicTooltip() {
+function translateTooltip() {
   // ショートカットありのツールチップ
   // .tooltip--notInteractive--2q4-q
   //   div.tooltip--content--3GEna
   //     span.tooltip--textWithShortcut--2r28_
   //       <span>Hand Tool</span>
-  const selector1 = 'div.tooltip--content--3GEna span span';
-  // Hand Tool, Text など固定のもの
-  _translateInnerHTML(selector1, tooltipMap);
-  // Rectangle, Pen など選択状況によって変わるもの
+  // Hand Tool, Text など固定のもの: tooltipMap
+  // Rectangle, Pen など選択状況によって変わるもの: toolsMap
   // 選択元のツールバーtoolsMapの翻訳を利用
-  _translateInnerHTML(selector1, toolsMap);
+  const shortcutTooltip = 'div.tooltip--content--3GEna span span';
 
   // ショートカットなしのツールチップ
   // .tooltip--notInteractive--2q4-q
   //   div.tooltip--content--3GEna
   //     <span...>Polygon</span>
-  const selector2 = 'div.tooltip--content--3GEna span';
-  // Edit Object など固定のもの
-  _translateInnerHTML(selector2, tooltipMap);
-  // Polygon, Start など選択状況によって変わるもの
+  // Edit Object など固定のもの: tooltipMap
+  // Polygon, Start など選択状況によって変わるもの: toolsMap
   // 選択元のツールバーtoolsMapの翻訳を利用
-  _translateInnerHTML(selector2, toolsMap);
+  const noshortcutTooltip = 'div.tooltip--content--3GEna span';
+
+  _translateInnerHTML(
+      shortcutTooltip+','+noshortcutTooltip,
+      tooltipMap.concat(toolsMap));
 }
 
-function translateDynamicShortcut() {
-  const selector1 = '.keyboard_shortcut_panel--tab--DuCac';
-  _translateInnerHTML(selector1, shortcutMap);
-  const selector2 = '.keyboard_shortcut_panel--caption--GQtJO';
-  _translateInnerHTML(selector2, shortcutMap);
-  const selector3 = '.keyboard_shortcut_panel--shortcutName--1cktE';
-  _translateInnerHTML(selector3, mainMenuMap);
-  _translateInnerHTML(selector3, toolsMap);
-  _translateInnerHTML(selector3, tooltipMap);
-  _translateInnerHTML(selector3, shortcutMap);
-  _translateInnerHTMLRegexp(selector3, shortcutRegexpMap);
-  const selector4 = '.keyboard_shortcut_panel--categoryCaption--34arC';
-  _translateInnerHTML(selector4, shortcutMap);
-}
+function translateShortcut() {
+  _translateInnerHTML(
+      '.keyboard_shortcut_panel--tab--DuCac,'+
+      '.keyboard_shortcut_panel--caption--GQtJO,'+
+      '.keyboard_shortcut_panel--categoryCaption--34arC',
+      shortcutMap);
 
-// 静的に(読み込み完了時に)生成されるメニューの翻訳
-function translateStaticMenu() {
-  // ツールバー class
-  const selector = '.toolbar_view--toolbar--2396w';
-  const toolbar = document.querySelector(selector);
-  // ツールバー読み込み完了待ち
-  if (!toolbar) {
-    window.setTimeout(translateStaticMenu, 100);
-    return;
-  }
-
-  // Share ボタン
-  const shareSelector = '.toolbar_view--shareButton--Q6fI7';
-  _translateInnerHTML(shareSelector, mainMenuMap);
-
-  // ツールチップ
-  const selector1 = '.chevron--chevronContainer--3xT09';
-  _translateDataTooltip(selector1, tooltipMap);
-
-  const selector2 = '.action--enabled--16Cku';
-  _translateDataTooltip(selector2, tooltipMap);
-
-  // パネル Layers, Assets, design, prototype, code
-  const selector3 = '.pages_panel--tab--3s1Y5';
-  _translateDataLabel(selector3, panelTabMap);
+  const selector = '.keyboard_shortcut_panel--shortcutName--1cktE';
+  _translateInnerHTML(selector,
+      mainMenuMap.concat(toolsMap, tooltipMap, shortcutMap));
+  _translateInnerHTMLRegexp(selector, shortcutRegexpMap);
 }
 
 // 動的に生成されるキーボードショートカットパネルの監視
-function observeDynamicShortcut() {
+function observeShortcutChange() {
   // 動的生成キーボードショートカットパネル要素の先祖 class (TODO 要検討)
-  const selector = 'div.fullscreen_view--flexContainer--3cbGo';
-  const target = document.querySelector(selector);
+  const target = document.querySelector(
+      'div.fullscreen_view--flexContainer--3cbGo');
   const config = {
     attributes: true,
     childList: true,
@@ -263,21 +200,20 @@ function observeDynamicShortcut() {
 
   // ページロード待ち
   if (!target) {
-    window.setTimeout(observeDynamicShortcut, 100);
+    window.setTimeout(observeShortcutChange, 100);
     return;
   }
 
   const observer = new MutationObserver(function() {
     // 動的生成キーボードショートカットパネル要素の先祖 class (TODO 要検討)
-    const selector =
-        'div.keyboard_shortcut_panel--keyboardShortcutPanel--2UT6R';
-    const targetShortcut = document.querySelector(selector);
+    const targetShortcut = document.querySelector(
+        'div.keyboard_shortcut_panel--keyboardShortcutPanel--2UT6R');
     if (targetShortcut) {
       // 変更検知の無限ループ回避
       observer.disconnect();
 
       // キーボードショートカットパネル生成検知時に翻訳する
-      translateDynamicShortcut();
+      translateShortcut();
 
       observer.observe(target, config);
     }
@@ -286,11 +222,10 @@ function observeDynamicShortcut() {
 }
 
 // 動的に生成されるツールチップの監視
-function observeDynamicTooltip() {
+function observeTooltipChange() {
   // 動的生成ツールチップ要素の先祖 class (TODO 要検討)
   // fullscreen_view--page--1QuyLの兄弟
-  const selector = '.tooltip--notInteractive--2q4-q';
-  const target = document.querySelector(selector);
+  const target = document.querySelector('.tooltip--notInteractive--2q4-q');
   const config = {
     attributes: true,
     childList: true,
@@ -300,28 +235,33 @@ function observeDynamicTooltip() {
   // ツールチップ表示完了待ち
   if (!target) {
     // ツールチップが初めて表示されるまでループし続ける
-    window.setTimeout(observeDynamicTooltip, 300);
+    window.setTimeout(observeTooltipChange, 300);
     return;
   }
-  translateDynamicTooltip();
+  translateTooltip();
 
   const observer = new MutationObserver(function() {
     // 変更検知の無限ループ回避
     observer.disconnect();
 
     // ツールチップ生成検知時に翻訳する
-    translateDynamicTooltip();
+    translateTooltip();
 
     observer.observe(target, config);
   });
   observer.observe(target, config);
 }
 
-// 動的に生成されるメニューの監視
-function observeDynamicMenu() {
-  // 動的生成メニュー要素の先祖 class (TODO 要検討)
-  const selector = '.fullscreen_view--page--1QuyL';
-  const menuTarget = document.querySelector(selector);
+function translatePage() {
+  translateToolbar();
+  translatePanel();
+  translateModal();
+}
+
+// 動的に生成されるページの監視
+function observePageChange() {
+  // 動的生成ページ要素の先祖 class (TODO 要検討)
+  const menuTarget = document.querySelector('.fullscreen_view--page--1QuyL');
   const config = {
     attributes: true,
     childList: true,
@@ -330,19 +270,17 @@ function observeDynamicMenu() {
 
   // ページ読み込み完了待ち
   if (!menuTarget) {
-    window.setTimeout(observeDynamicMenu, 100);
+    window.setTimeout(observePageChange, 100);
     return;
   }
+  translatePage();
 
   const menuObserver = new MutationObserver(function() {
     // 変更検知の無限ループ回避
     menuObserver.disconnect();
 
-    // メニュー生成検知時に翻訳する
-    translateDynamicMainMenu();
-    translateDynamicTools();
-    translateDynamicPanel();
-    translateDynamicModal();
+    // ページ要素生成検知時に翻訳する
+    translatePage();
 
     menuObserver.observe(menuTarget, config);
   });
@@ -350,27 +288,25 @@ function observeDynamicMenu() {
 }
 
 function initialize() {
-  translateStaticMenu();
-  observeDynamicMenu();
-  observeDynamicTooltip();
-  observeDynamicShortcut();
+  observePageChange();
+  observeTooltipChange();
+  observeShortcutChange();
 }
 
 /* ページ遷移の監視
  * 初回ロード時はタイミングの関係で実行されない(TODO 要検証)
  */
-function observePage() {
+function observePageTransition() {
   // ページ遷移時に変化する要素の親要素 class (TODO 要検討)
-  const selector = '.in_app_page--content--2V_He';
-  const page = document.querySelector(selector);
+  const page = document.querySelector('.in_app_page--content--2V_He');
 
   // ページ読み込み完了待ち
   if (!page) {
-    window.setTimeout(observePage, 100);
+    window.setTimeout(observePageTransition, 100);
     return;
   }
   const pageObserver = new MutationObserver(function() {
-    // ページ遷移後、再度メニューを監視する
+    // ページ遷移後、再度ページを監視する
     initialize();
   });
   pageObserver.observe(page, {
@@ -378,14 +314,14 @@ function observePage() {
     childList: true,
   });
 }
-observePage();
+observePageTransition();
 
 /* 初回ロード完了時に実行
- * ページ遷移時は実行されない(TODO 検証)
+ * ページ遷移時は実行されない
  */
 document.addEventListener('readystatechange', (event) => {
   if (event.target.readyState === 'complete') {
-    // ロード完了後、メニュー監視
+    // ロード完了後、ページ監視
     initialize();
   }
 });
