4990e06ca8a121f4145fd76950b2d66e8eda435c
Jun Kawashima <5635944+junkawa@users.noreply.github.com>
2020-09-25 19:53:37 +0900

    Modify publish task

--- tools/publish.sh
+++ tools/publish.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
-set -eux
+set -eu
+
+# Get ready
+#CLIENT_ID=''
+#CLIENT_SECRET=''
+#CODE=''
+#echo "https://accounts.google.com/o/oauth2/auth?response_type=code&scope=https://www.googleapis.com/auth/chromewebstore&client_id=$CLIENT_ID&redirect_uri=urn:ietf:wg:oauth:2.0:oob"
+#curl "https://accounts.google.com/o/oauth2/token" -d \
+#    "client_id=$CLIENT_ID&client_secret=$CLIENT_SECRET&code=$CODE&grant_type=authorization_code&redirect_uri=urn:ietf:wg:oauth:2.0:oob"
+#REFRESH_TOKEN=''
 
 # expect to call from .github/workflows/release.yml
 
@@ -7,11 +16,9 @@ APP_ID='ekodckpnalonppfcddgnkoapajahbahc'
 
 # Get Access Token for Chrome web store API
 # https://developers.google.com/identity/protocols/oauth2/web-server#httprest_7
-#ACCESS_TOKEN=$(curl --data "refresh_token=$REFRESH_TOKEN" --data "client_id=$CLIENT_ID" --data "client_secret=$CLIENT_SECRET" --data "grant_type=refresh_token" https://oauth2.googleapis.com/token | jq -r '.access_token')
-ACCESS_TOKEN=$(curl --data "refresh_token=$REFRESH_TOKEN" --data "client_id=$CLIENT_ID" --data "client_secret=$CLIENT_SECRET" --data "grant_type=refresh_token" https://www.googleapis.com/oauth2/v4/token | jq -r '.access_token')
+ACCESS_TOKEN=$(curl -s --data "refresh_token=$REFRESH_TOKEN" --data "client_id=$CLIENT_ID" --data "client_secret=$CLIENT_SECRET" --data "grant_type=refresh_token" https://oauth2.googleapis.com/token | jq -r '.access_token')
 
 # Uploading a package and Publishing an item to the public
 # https://developer.chrome.com/webstore/using_webstore_api
-#curl -H "Authorization: Bearer $ACCESS_TOKEN" -H "x-goog-api-version: 2" -X PUT -T $FILE_NAME -v https://www.googleapis.com/upload/chromewebstore/v1.1/items/$APP_ID
-#curl -H "Authorization: Bearer $ACCESS_TOKEN" -H "x-goog-api-version: 2" -H "Content-Length: 0" -X POST -v https://www.googleapis.com/chromewebstore/v1.1/items/$APP_ID/publish
-curl https://accounts.google.com/o/oauth2/revoke?token=$ACCESS_TOKEN
+curl -s -H "Authorization: Bearer $ACCESS_TOKEN" -H "x-goog-api-version: 2" -X PUT -T $FILE_NAME -v https://www.googleapis.com/upload/chromewebstore/v1.1/items/$APP_ID
+curl -s -H "Authorization: Bearer $ACCESS_TOKEN" -H "x-goog-api-version: 2" -H "Content-Length: 0" -X POST -v https://www.googleapis.com/chromewebstore/v1.1/items/$APP_ID/publish
