820f25c053f03aaddcfcd750f5e32b15c72b24f6
Jun Kawashima <5635944+junkawa@users.noreply.github.com>
2020-11-19 23:32:41 +0900

    Move manifest.json to figma_jp/

--- tools/build.sh
+++ tools/build.sh
@@ -9,10 +9,4 @@ if [ -d $OUTPUT_DIR ]; then
 fi
 mkdir $OUTPUT_DIR
 
-cp -a $SRC_DIR manifest.json $OUTPUT_DIR
-
-#VERSION=$(cat ./manifest.json | grep \"version\" |
-#    sed -e "s/^.*\"version\":.*\"\(.*\)\",.*/\1/g")
-#PACKAGE_DIR=$OUTPUT_DIR/"figma_jp_"$VERSION"_"$(date "+%Y%m%d%H%M")
-#mkdir $PACKAGE_DIR
-#cp -a $SRC_DIR manifest.json $PACKAGE_DIR
+cp -a ${SRC_DIR}/* ${OUTPUT_DIR}/
--- tools/release.sh
+++ tools/release.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -xeu
 
-VERSION=v$(cat ./manifest.json | grep \"version\" |
+VERSION=v$(cat ./figma_jp/manifest.json | grep \"version\" |
     sed -e "s/^.*\"version\":.*\"\(.*\)\",.*/\1/g")
-#VERSION=v$(cat ./manifest.json | jq -r '.version')
+#VERSION=v$(cat ./figma_jp/manifest.json | jq -r '.version')
 
 git tag $VERSION
 git push origin $VERSION
