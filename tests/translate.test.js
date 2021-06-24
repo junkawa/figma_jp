6a97ae0d4fea5a73369299b63b5aa95602b5d512
Jun Kawashima <5635944+junkawa@users.noreply.github.com>
2021-06-24 10:11:56 +0900

    Fix test for menu updates

--- tests/translate.test.js
+++ tests/translate.test.js
@@ -76,7 +76,7 @@ const mainMenuSelector = (() => {
     Edit: menuBase + ' > div:nth-child(5)',
     'Edit > Copy as': menu2ndBase + ' > div:nth-child(4)',
     View: menuBase + ' > div:nth-child(6)',
-    'View > Panels': menu2ndBase + ' > div:nth-child(14)',
+    'View > Panels': menu2ndBase + ' > div:nth-child(12)',
     Object: menuBase + ' > div:nth-child(7)',
     'Object > Main Component': menu2ndBase + ' > div:nth-child(11)',
     'Object > Boolean Groups': menu2ndBase + ' > div:nth-child(27)',
@@ -317,6 +317,7 @@ mainMenuMap['View'] = [
     en: 'Rulers',
     ja: '定規を表示',
   },
+  /*
   {
     en: 'Show Slices',
     ja: 'スライスを表示',
@@ -325,6 +326,7 @@ mainMenuMap['View'] = [
     en: 'Comments',
     ja: 'コメントを表示',
   },
+  */
   {
     en: 'Outlines',
     ja: 'アウトラインを表示',
